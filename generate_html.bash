555ff241579d9703d01eb30d1cd441581db03ddf
Whyjay Zheng <wz278@cornell.edu>
2021-02-04 21:39:25 -0500

    Add .pot files to the repo

--- generate_html.bash
+++ generate_html.bash
@@ -1,9 +1,9 @@
 # routine to generate htmls
 # by Whyjay, Jul 13 2016
 
-# make gettext
-# sphinx-intl update -p _build/locale -l en
-# zanata-cli push
+make gettext
+sphinx-intl update -p _build/locale -l en
+zanata-cli push
 zanata-cli pull
 make html
 make -e SPHINXOPTS="-D language='en_US'" BUILDSURFIX="/en" html
