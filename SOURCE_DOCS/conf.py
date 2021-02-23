57cc8df8ef5a16b0c58b3928b3434e9dd68b9362
Whyjay Zheng <wz278@cornell.edu>
2021-02-23 12:00:46 -0500

    Add missing locale strings back (code blocks)

--- SOURCE_DOCS/conf.py
+++ SOURCE_DOCS/conf.py
@@ -357,4 +357,7 @@ epub_exclude_files = ['search.html']
 # If false, no index is generated.
 #epub_use_index = True
 
+# -- Options for i18n (sphinx-intl) ----------------------------------------------
 locale_dirs = ['locale/']
+gettext_compact = False
+gettext_additional_targets = ['literal-block']
