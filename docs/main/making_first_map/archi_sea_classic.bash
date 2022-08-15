fa8b15b227d77886e5e3e518e327cc07362984bb
Whyjay Zheng <whyjz@berkeley.edu>
2022-08-15 06:39:09 +0000

    Update notebook materials for making_first_map

--- docs/main/making_first_map/archi_sea_classic.bash
+++ docs/main/making_first_map/archi_sea_classic.bash
@@ -1,1 +1,2 @@
-pscoast -R19.42/22.95/59.71/60.56 -JM6i -W0.1p,black -Gdarkseagreen2 -Scornflowerblue -Df -P -Ba2f0.5g1 -BWSne+t"Archipelago Sea" -A0.1 -U > archi_sea.ps
+gmt pscoast -R19.42/22.95/59.71/60.56 -JM6i -W0.1p,black -Gdarkseagreen2 -Scornflowerblue -Df -P -Ba2f0.5g1 -BWSne+t"Archipelago Sea" -A0.1 -U > archi_sea_classic.ps
+gmt psconvert archi_sea_classic.ps -A -Tg 
--- docs/main/making_first_map/archi_sea_gmt6.bash
+++
@@ -1,3 +0,0 @@
-gmt coast -R19.42/22.95/59.71/60.56 -JM6i -W0.1p,black -Gdarkseagreen2 \
-          -Scornflowerblue -Ba2f0.5g1 -BWSne+t"Archipelago Sea" -A0.1 -U \
-          -png archi_sea_gmt6
